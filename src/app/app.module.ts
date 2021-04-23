d0504c5e0ee88e8dc8554790f40015fda9bdb02b
heronet <sirat4757@gmail.com>
2021-04-23 18:21:36 +0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MessagesComponent } from './core/messages/messages.component';
+import { ConversationComponent } from './core/conversation/conversation.component';
+import { ToolbarComponent } from './core/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MessagesComponent,
+    ConversationComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
